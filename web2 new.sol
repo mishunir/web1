c70004e5c0c293258253e64c4633e5cf911b022d
mishunir <koushiknir5@gmail.com>
2025-09-26 12:34:40 +0600

    web

---
+++ web2 new.sol
@@ -0,0 +1,1 @@
+// SPDX-License-Identifier: MITpragma solidity ^0.8.20;contract WebStorage {    string private data;    // Save data    function setData(string memory _data) public {        data = _data;    }    // Retrieve data    function getData() public view returns (string memory) {        return data;    }}
